25b6c0fd167b9f0d39e9cdd2f009a42e49cc6207
Colin Floss <colin.floss@lakehouse.digital>
2022-05-23 14:03:42 -0400

    change

---
+++ .output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,13 @@
+const client_manifest = {
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "file": "entry-3538e376.mjs",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "css": [
+      "entry.27253da5.css"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
---
+++ .output/server/chunks/nitro/static.mjs
@@ -0,0 +1,101 @@
+import { eventHandler, createError } from 'h3';
+import { withLeadingSlash, withoutTrailingSlash, parseURL } from 'ufo';
+import { promises } from 'fs';
+import { resolve, dirname } from 'pathe';
+import { fileURLToPath } from 'url';
+
+const assets = {
+  "/_nuxt/entry-3538e376.mjs": {
+    "type": "application/javascript",
+    "etag": "\"e1abc-QLsHR/g81ZyjFA9cba2vKU0JfHc\"",
+    "mtime": "2022-05-23T17:20:36.901Z",
+    "path": "../public/_nuxt/entry-3538e376.mjs"
+  },
+  "/_nuxt/entry.27253da5.css": {
+    "type": "text/css; charset=utf-8",
+    "etag": "\"1f41-Ufa06yzjDp1S2DRvyPn/YuAI564\"",
+    "mtime": "2022-05-23T17:20:36.901Z",
+    "path": "../public/_nuxt/entry.27253da5.css"
+  },
+  "/_nuxt/manifest.json": {
+    "type": "application/json",
+    "etag": "\"cb-1d6inUw0z+G0Bm7xEhs1otYngUc\"",
+    "mtime": "2022-05-23T17:20:36.901Z",
+    "path": "../public/_nuxt/manifest.json"
+  }
+};
+
+const mainDir = dirname(fileURLToPath(globalThis.entryURL));
+function readAsset (id) {
+  return promises.readFile(resolve(mainDir, assets[id].path)).catch(() => {})
+}
+
+const publicAssetBases = ["/_nuxt"];
+
+function isPublicAssetURL(id = '') {
+  if (assets[id]) {
+    return
+  }
+  for (const base of publicAssetBases) {
+    if (id.startsWith(base)) { return true }
+  }
+  return false
+}
+
+function getAsset (id) {
+  return assets[id]
+}
+
+const METHODS = ["HEAD", "GET"];
+const _static = eventHandler(async (event) => {
+  if (event.req.method && !METHODS.includes(event.req.method)) {
+    return;
+  }
+  let id = decodeURIComponent(withLeadingSlash(withoutTrailingSlash(parseURL(event.req.url).pathname)));
+  let asset;
+  for (const _id of [id, id + "/index.html"]) {
+    const _asset = getAsset(_id);
+    if (_asset) {
+      asset = _asset;
+      id = _id;
+      break;
+    }
+  }
+  if (!asset) {
+    if (isPublicAssetURL(id)) {
+      throw createError({
+        statusMessage: "Cannot find static asset " + id,
+        statusCode: 404
+      });
+    }
+    return;
+  }
+  const ifNotMatch = event.req.headers["if-none-match"] === asset.etag;
+  if (ifNotMatch) {
+    event.res.statusCode = 304;
+    event.res.end("Not Modified (etag)");
+    return;
+  }
+  const ifModifiedSinceH = event.req.headers["if-modified-since"];
+  if (ifModifiedSinceH && asset.mtime) {
+    if (new Date(ifModifiedSinceH) >= new Date(asset.mtime)) {
+      event.res.statusCode = 304;
+      event.res.end("Not Modified (mtime)");
+      return;
+    }
+  }
+  if (asset.type) {
+    event.res.setHeader("Content-Type", asset.type);
+  }
+  if (asset.etag) {
+    event.res.setHeader("ETag", asset.etag);
+  }
+  if (asset.mtime) {
+    event.res.setHeader("Last-Modified", asset.mtime);
+  }
+  const contents = await readAsset(id);
+  event.res.end(contents);
+});
+
+export { _static as default };
+//# sourceMappingURL=static.mjs.map
